0ae19e1d705df7985c5b4780ca99bac9bd41b5d6
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-09-03 15:51:14 +0100

    Set up basic index layout.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  { text: "Hello", user: "Andy", added: new Date() },
+  { text: "Hi there!", user: "Sam", added: new Date() },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", {
+    title: "Welcome",
+    messages: messages,
+  });
 });
 
 module.exports = router;
